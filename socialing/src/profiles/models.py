d4d4a353e6ca733e30dd56e9405f3946bd7238c6
BekovaDeniza <bekova_02@inbox.ru>
2022-06-26 16:47:22 +0300

    create custom user

--- socialing/src/profiles/models.py
+++ socialing/src/profiles/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserNet(AbstractUser):
+    """Custom user model
+        """
+    GENDER = (
+        ('male', 'male'),
+        ('female', 'female'),
+        ('other', 'other')
+    )
+    middle_name = models.CharField(max_length=50)
+    first_login = models.DateTimeField(blank=True, null=True)
+    phone = models.CharField(max_length=14)
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='user/avatar/', blank=True, null=True)
+    tg = models.CharField(max_length=500, blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=6, choices=GENDER, default='other')
