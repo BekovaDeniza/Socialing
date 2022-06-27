af79c242f1f0015e8f3c8c7df4d68c685fa0f6de
BekovaDeniza <bekova_02@inbox.ru>
2022-06-27 05:38:27 +0300

    update djoser, simple_jwt and added update, get user profile

--- socialing/src/profiles/serializers.py
+++ socialing/src/profiles/serializers.py
@@ -4,8 +4,38 @@ from .models import UserNet
 
 
 class GetUserNetSerializer(serializers.ModelSerializer):
-    """Вывод инфо о пользователе
+    """ Вывод инфо о user
     """
+    avatar = serializers.ImageField(read_only=True)
+
     class Meta:
         model = UserNet
-        exclude = ("password", "last_login", "is_active", "is_staff", "is_superuser")
+        exclude = (
+            "password",
+            "last_login",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions"
+        )
+
+
+class GetUserNetPublicSerializer(serializers.ModelSerializer):
+    """ Вывод публичной информации о user
+    """
+    avatar = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = UserNet
+        exclude = (
+            "email",
+            "password",
+            "phone",
+            "last_login",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions"
+        )
--- socialing/src/profiles/views.py
+++ socialing/src/profiles/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
-from rest_framework.generics import RetrieveAPIView
-
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
 from .models import UserNet
-from .serializers import GetUserNetSerializer
+from .serializers import GetUserNetSerializer, GetUserNetPublicSerializer
 
 
-class GetUserNetView(RetrieveAPIView):
-    """Вывод профиля пользователя
+class UserNetPublicView(ModelViewSet):
+    """Вывод публичного профиля пользователя
     """
     queryset = UserNet.objects.all()
-    serializer_class = GetUserNetSerializer
+    serializer_class = GetUserNetPublicSerializer
+    permission_classes = [permissions.AllowAny]
 
+
+class UserNetView(ModelViewSet):
+    """Вывод профиля пользователя
+    """
+    serializer_class = GetUserNetSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return UserNet.objects.filter(id=self.request.user.id)
